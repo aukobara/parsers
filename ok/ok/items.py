dae5c95f0abcb32c577b9d0ac2e6c99077ccf5db
Oleg Kulikov <aukobara@gmail.com>
2015-04-24 02:17:12 +0300

    Comment out paging due to AJAX bug - need to re-implement Commit ProductItem to pipeline at PDP parser only. Grid just return it in Request.meta Implement parsePDP  - link now is cannonical URL with internal prodID, link from crawler in separate field Parse key-values details on PDP as 'details' field which is serialized in JSON format

--- ok/ok/items.py
+++ ok/ok/items.py
@@ -4,9 +4,9 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
+import json
 
 import scrapy
-
 
 class CatItem(scrapy.Item):
     # define the fields for your item here like:
@@ -17,9 +17,12 @@ class CatItem(scrapy.Item):
 
 class ProductItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    id = scrapy.Field() # unique ProductID which can be used for PDP request by canonical url
     name = scrapy.Field()
-    imgUrl = scrapy.Field()
-    price = scrapy.Field()
-    link = scrapy.Field()
+    imgUrl = scrapy.Field() # small image for grid
+    price = scrapy.Field() # offer price
+    link = scrapy.Field() # canonical unique universal link to PDP, without referrers, cats and excessive params
+    crawllink = scrapy.Field() # link to PDP reachable by crawler from category, can be different than canonical
     weight = scrapy.Field()
+    # JSON with key-values on PDP
+    details = scrapy.Field(serializer=lambda map: json.dumps(map, ensure_ascii=False).encode("utf-8"))
--- ok/ok/spiders/ok_cat_root.py
+++ ok/ok/spiders/ok_cat_root.py
@@ -1,6 +1,9 @@
 # coding: cp1251
+from urlparse import urlunparse
 import scrapy
 from scrapy.http.response.text import TextResponse
+from scrapy.log import ERROR
+from scrapy.utils.url import parse_url
 from ok.items import CatItem, ProductItem
 import re
 
@@ -14,32 +17,19 @@ import re
 class RootCatSpider(scrapy.Spider):
     name = "ok"
     allowed_domains = ["okeydostavka.ru"]
-    start_urls = ["http://www.okeydostavka.ru/msk/catalog"]
-    # start_urls = ["http://www.okeydostavka.ru/msk/%D0%BC%D0%BE%D0%BB%D0%BE%D1%87%D0%BD%D1%8B%D0%B5-%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82%D1%8B--%D1%81%D1%8B%D1%80%D1%8B--%D1%8F%D0%B9%D1%86%D0%BE/%D0%BC%D0%BE%D0%BB%D0%BE%D1%87/%D1%81%D0%BC%D0%B5%D1%82%D0%B0%D0%BD%D0%B0"]
+    # start_urls = ["http://www.okeydostavka.ru/msk/catalog"]
+    start_urls = ["http://www.okeydostavka.ru/msk/%D0%BC%D0%BE%D0%BB%D0%BE%D1%87%D0%BD%D1%8B%D0%B5-%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82%D1%8B--%D1%81%D1%8B%D1%80%D1%8B--%D1%8F%D0%B9%D1%86%D0%BE/%D0%BC%D0%BE%D0%BB%D0%BE%D1%87/%D0%B9%D0%BE%D0%B3%D1%83%D1%80%D1%82%D1%8B-%D0%B8-%D1%82%D0%B2%D0%BE%D1%80%D0%BE%D0%B6%D0%BA%D0%B8"]
 
     def __init__(self):
+        super(RootCatSpider, self).__init__()
         with open("catlog.txt", "w") as f:
             f.truncate()
-        # self.download_delay = 1/2
+            # self.download_delay = 1/2
 
     def extractA(self, selA):
         text = selA.xpath("text()").extract()[0].strip()
         url = selA.xpath("@href").extract()[0]
         return url, text
-
-    def extractProd(self, selP):
-        if not selP.xpath(".//div[@class='product_name']/a/text()").extract():
-            return None
-
-        item = ProductItem()
-        item ["name"] = selP.xpath(".//div[@class='product_name']/a/text()").extract()[0].strip()
-        item ["link"] = selP.xpath(".//div[@class='product_name']/a/@href").extract()[0]
-        item ["imgUrl"] = selP.xpath(".//div[@class='image']//img/@src").extract()[0]
-        item ["weight"] = ' '.join([txt.strip() for txt in selP.xpath(".//div[@class='product_weight']/span/text()").extract()])
-        selPrice = selP.xpath(".//span[@class='product_price']/span/text()")
-        item ["price"] = selPrice.extract()[0].strip() if selPrice else None
-        return item
-
 
     def catlog(self, level, item):
         with open("catlog.txt", "ab") as f:
@@ -68,12 +58,19 @@ class RootCatSpider(scrapy.Spider):
         for link in pageLinks:
             yield scrapy.Request(link, self.parse)
 
-        for prodSel in response.xpath("//div[@class='product_listing_container']//div[contains(concat(' ', @class, ' '), ' product ')]"):
-            item = self.extractProd(prodSel)
+        prodContSel = response.xpath("//div[@class='product_listing_container']")
+        for prodSel in prodContSel.css("div.product"):
+            item = self.parseProductThumbnail(prodSel)
             if item:
-                yield item
-                if item["link"]:
-                    yield scrapy.Request(item["link"], callback=self.parseProductDetails)
+                # yield item
+                if item["crawllink"]:
+                    pdpRequest = scrapy.Request(item["crawllink"], callback=self.parseProductDetails)
+                    pdpRequest.meta["prodItem"] = item
+                    yield pdpRequest
+                else:
+                    self.log("Link is empty or unparsed to PDP on page %r for product %r" %
+                               (response.url, item["name"]), level=ERROR)
+                    yield item # return broken uncompleted product
 
     def parseProductListPaging(self, response):
         """
@@ -89,6 +86,7 @@ class RootCatSpider(scrapy.Spider):
         Link format: {cat-url}#facet:&productBeginIndex:72&orderBy:2&pageView:grid&minPrice:&maxPrice:&pageSize:&
         """
         pageLinks = set()
+        """ REWRITE TO USE POST AJAX REQUESTS
         pageMenuSel = response.xpath("//div[@class='pageControlMenu']")
         if pageMenuSel:
             baseUrl = response.request.url
@@ -102,13 +100,59 @@ class RootCatSpider(scrapy.Spider):
                 pageSize = int(jslinkSel.re("pageSize:\"(\\d+)\"")[0])
                 link = re.sub(r"productBeginIndex:\d+", "productBeginIndex:%d" % ((pageNumber-1) * pageSize), baseUrl)
                 pageLinks.add( link )
+        """
         return pageLinks
+
+    def parseProductThumbnail(self, selP):
+        """
+        Extract ProductItem from category product's grid's topmost <div class='product...>
+        @param selP: Selector to <div class='product...'>
+        @type selP: TextResponse
+        @return: ProductItem
+        Sample at ./product_item.xml
+        """
+        if not selP.xpath(".//div[@class='product_name']/a/text()").extract():
+            return None
+
+        item = ProductItem()
+        item ["name"] = selP.xpath(".//div[@class='product_name']/a/@title").extract()[0].strip()
+        item ["crawllink"] = selP.xpath(".//div[@class='product_name']/a/@href").extract()[0]
+        item ["imgUrl"] = selP.xpath(".//div[@class='image']//img/@src").extract()[0]
+        item ["weight"] = ' '.join([txt.strip() for txt in selP.xpath(".//div[@class='product_weight']/span/text()").extract()])
+        selPrice = selP.css("span.product_price span.price.label")
+        item ["price"] = selPrice.xpath("text()").extract()[0].strip() if selPrice else None
+        return item
 
     def parseProductDetails(self, response):
         """
         Parse page with Product key-values
         @type response: TextResponse
         @param response Original HTTP Response
+        response.meta["prodItem"] contains ProductItem from category grid
         """
-        pass
+        item = response.meta["prodItem"]
+        """@type item: ProductItem"""
+        entItemJsonSel = response.xpath("//div[starts-with(@id, 'entitledItem_')]/text()")
+        productId = int( entItemJsonSel.re("\"catentry_id\"\s+:\s+\"(\d+)\",")[0] )
+        item ["id"] = productId
+        parsedUrl = parse_url(response.url)
+        item ["link"] = urlunparse((parsedUrl.scheme, parsedUrl.netloc.lower(), "webapp/wcs/stores/servlet/ProductDisplay", "",
+                                "urlRequestType=Base&productId=%d&storeId=10151" % productId, ""))
+
+        details = self.parsePDPkeyvalues(response)
+        item ["details"] = details
+        yield item
+
+    def parsePDPkeyvalues(self, response):
+        """
+        @type response: TextResponse
+        @return: dict
+        """
+        trSelList = response.css("div.product-characteristics tr,div.product-desc tr")
+        details = {}
+        for tr in trSelList:
+            key = " ".join(txt.strip() for txt in tr.xpath("th//text()").extract())
+            value = " ".join(txt.strip() for txt in tr.xpath("td//text()").extract())
+            details[key] = value
+        return details
 
