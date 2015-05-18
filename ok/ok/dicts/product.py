5c00a78e53f8d80efd86350b65b523aa78afabfa
Oleg Kulikov <aukobara@gmail.com>
2015-05-18 23:42:47 +0300

    raw_item as property; raw_id - virtual attribute with delegation to raw_item to/from_csv - support tags

--- ok/ok/dicts/product.py
+++ ok/ok/dicts/product.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+from UserDict import DictMixin
 import csv
+
+PRODUCT_ATTRIBUTE_RAW_ID = 'raw_id'
 
 
 class Product(dict):
+
+    # Product class itself should not have any fields except dictionary data
+    __slots__ = ()
+    validators = dict()
+    """@type: dict of (str, callable)"""
 
     @property
     def pfqn(self):
@@ -26,6 +34,45 @@ class Product(dict):
     def sqn(self, sqn):
         self["sqn"] = sqn
 
+    @property
+    def raw_item(self):
+        """
+        @rtype: dict
+        """
+        return self.get('raw_item', dict())
+
+    def _validate_raw_item(self, raw_item):
+        if not isinstance(raw_item, (dict, DictMixin)):
+            raise Exception('Raw_item must be dict but %s comes to Product[pfqn=%s]' % (type(raw_item), self.pfqn))
+    validators['raw_item'] = _validate_raw_item
+
+    @raw_item.setter
+    def raw_item(self, raw_item):
+        self["raw_item"] = raw_item
+
+    def __contains__(self, key):
+        if key == PRODUCT_ATTRIBUTE_RAW_ID:
+            return 'id' in self.raw_item
+        return dict.__contains__(self, key)
+
+    def __getitem__(self, key):
+        if key == PRODUCT_ATTRIBUTE_RAW_ID and 'id' in self.raw_item:
+            return self.raw_item['id']
+        return dict.__getitem__(self, key)
+
+    def _validate(self, key, value):
+        if key in self.validators:
+            self.validators[key](self, value)
+
+    def __setitem__(self, key, value):
+        self._validate(key, value)
+        dict.__setitem__(self, key, value)
+
+    def update(self, other=None, **kwargs):
+        for k, v in (other or kwargs).iteritems():
+            self._validate(k, v)
+        dict.update(self, other, **kwargs)
+
     @staticmethod
     def to_meta_csv(csv_filename, products):
         """
@@ -35,12 +82,18 @@ class Product(dict):
         """
         with open(csv_filename, 'wb') as f:
             f.truncate()
-            header = ['raw_id', 'sqn', 'brand', 'brand_detected', 'product_manufacturer', 'weight', 'fat', 'pack', 'pfqn']
+            header = [PRODUCT_ATTRIBUTE_RAW_ID, 'sqn', 'brand', 'brand_detected', 'product_manufacturer', 'weight', 'fat', 'pack', 'pfqn', 'tags']
             writer = csv.DictWriter(f, header)
             writer.writeheader()
             for product in products:
-                d = {field: unicode(product[field]).encode("utf-8") for field in header if product.get(field)}
-                d['raw_id'] = product["raw_item"]["id"]
+                d = dict()
+                for field in header:
+                    if field in product:
+                        value = product[field]
+                        if isinstance(value, (list, tuple)):
+                            value = u'|'.join(value)
+                        d[field] = unicode(value).encode("utf-8")
+
                 writer.writerow(d)
 
         pass
@@ -59,5 +112,7 @@ class Product(dict):
             for row in reader:
                 product_meta_row = dict(zip(fields, row))
                 product = Product(**{field: value.decode("utf-8") for field, value in product_meta_row.iteritems()})
+                if 'tags' in product and product.get('tags') is not None:
+                    product['tags'] = product['tags'].split(u'|')
                 yield product
         pass
