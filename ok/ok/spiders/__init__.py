11d73e8e66a66d6b23c42a7311064ffebbcd43b9
Oleg Kulikov <aukobara@gmail.com>
2015-05-27 05:26:01 +0300

    add fix_url() to restore relative paths in links

--- ok/ok/spiders/__init__.py
+++ ok/ok/spiders/__init__.py
@@ -2,3 +2,16 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+from urlparse import urlparse, urljoin
+
+
+def fix_url(href, response):
+    """
+    @param scrapy.http.response.html.HtmlResponse response: response
+    """
+    url_p = urlparse(href)
+    if not url_p.scheme and not href.startswith('/'):
+        # Fix relative path
+        href = '/' + href
+    cat_url = urljoin(response.url, href)
+    return cat_url
