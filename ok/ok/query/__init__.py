74ff1b6e2d0ed1cbcd09f77844547dcbc30c9835
Oleg Kulikov <aukobara@gmail.com>
2015-07-14 09:16:36 +0300

    parse_query(): - return EmptyQuery for empty string - instantiate correct class for Query slices

--- ok/ok/query/__init__.py
+++ ok/ok/query/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, absolute_import
+from ok.query.tokens import QueryItemBase
 
 """
 This is a package for query processing modules like tokenizer, parsing context, etc
@@ -7,5 +8,14 @@ This is a package for query processing modules like tokenizer, parsing context, 
 
 
 def parse_query(q_str, predecessor_query=None, lowercase=True):
-    from ok.query.tokens import DefaultQuery, Query
-    return q_str if isinstance(q_str, Query) else DefaultQuery(q_str, predecessor_query=predecessor_query, lowercase=lowercase)
+    """@rtype: ok.query.tokens.Query"""
+    from ok.query.tokens import DefaultQuery, Query, EmptyQuery
+    if not q_str:
+        return EmptyQuery
+    elif isinstance(q_str, Query):
+        return q_str
+    elif isinstance(q_str, list) and q_str and isinstance(q_str[0], QueryItemBase) and q_str[0].query is not None:
+        query_type = q_str[0].query.__class__
+        return query_type(q_str, predecessor_query=predecessor_query, lowercase=lowercase)
+    else:
+        return DefaultQuery(q_str, predecessor_query=predecessor_query, lowercase=lowercase)
